c934612b9363a8e90b865c63ebd8a7c6e47943f2
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2023-08-22 23:42:50 +0800

    first

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using WebsiteImages;
+
+namespace WebsiteImages;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please Enter URL");
+            return;
+        }
+
+        string url = args[0];
+
+        Console.WriteLine($"URL: {url}");
+        
+        WebsiteImagesFinder finder = new(url);
+
+        foreach (var img in finder.GetImageUrls())
+        {
+            Console.WriteLine(img);
+        }
+
+    }
+}
---
+++ WebsiteImagesFinder.cs
@@ -0,0 +1,86 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+namespace WebsiteImages
+{
+    public class WebsiteImagesFinder
+    {
+        public string Host { get; set; }
+        public string Url { get; set; }
+        public WebClient Client { get; set; }
+
+        public WebsiteImagesFinder(string url)
+        {
+            Url = url;
+            Client = new();
+
+            Uri uri = new(url);
+            Host = uri.Host;
+        }
+
+        public string GetContent()
+        {
+            return Client.DownloadString(Url);
+        }
+
+        public int ImgTagCount()
+        {
+            string content = Client.DownloadString(Url);
+            string target = "<img";
+
+            int count = 0;
+            int index = 0;
+
+
+            while ((index = content.IndexOf(target, index)) != -1)
+            {
+                index += target.Length;
+                count++;
+            }
+
+            return count;
+        }
+
+        public HashSet<string> GetImageUrls()
+        {
+            string pattern = @"<img[^>]*\s+src\s*=\s*[""']([^""']+)[""'][^>]*>";
+            string content = Client.DownloadString(Url);
+
+            MatchCollection matches = Regex.Matches(content, pattern);
+
+            HashSet<string> urls = new();
+
+            foreach (Match match in matches.Cast<Match>())
+            {
+                string url = match.Groups[1].Value;
+
+                if (url[0] == '/')
+                {
+                    url = $"https://{Host}{url}";
+                }
+
+                urls.Add(url);
+            }
+
+            return urls;
+        }
+
+        public async Task DownloadFileAsync(string url, string outputPath)
+        {
+            HttpClient client = new();
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Stream stream = await response.Content.ReadAsStreamAsync();
+
+                FileStream fileStream = File.Create(outputPath);
+                await stream.CopyToAsync(fileStream);
+            }
+            else
+            {
+                Console.WriteLine($"Failed to download file. Status code: {response.StatusCode}");
+            }
+        }
+    }
+}
